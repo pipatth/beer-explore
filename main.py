8b2d315c9bfc549dcf67e7952fdf45f5543c2012
pipat <pipat.th@gmail.com>
2015-08-10 12:04:24 -0500

    change

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import beerLoadData
-import beer_similarity
+import beerSimilarity
+import sys
 
 def find_similar_beers(beerid, styleid):
     # get top 20 beers in style
@@ -10,13 +11,21 @@ def find_similar_beers(beerid, styleid):
     df = beerLoadData.load_reviews(list_beer)
 
     # get correlations
-    list_sim = [beer_similarity.calc_similarity(df, beerid, x) for x in list_beer2]
+    list_sim = [beerSimilarity.calc_similarity(df, beerid, x) for x in list_beer2]
     list_sim.sort(key = lambda x: x[1], reverse=True)
 
     # convert to pandas df
     df = beerLoadData.to_pd_table(list_sim)
     return df
 
-def find_similar_beers_from_name(beername, breweryname, styleid):
+def find_similar_beers_from_name(beername, breweryname, stylename):
     beerid = beerLoadData.get_beer_id(beername, breweryname)
-    return find_similar_beers(beerid, styleid)
+    return find_similar_beers(beerid, beerLoadData.get_style_id(stylename))
+
+def main(argv):
+    print argv[3]
+    print find_similar_beers_from_name(argv[1], argv[2], argv[3])
+    return
+
+if __name__ == '__main__':
+    main(sys.argv)
